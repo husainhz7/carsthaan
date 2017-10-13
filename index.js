60159417dd9cde2b756e21c6fd04cef2b07a5ea0
Husain Zaidi <husainhz7@gmail.com>
2017-10-13 21:57:46 +0000

    added ejs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
+app.set('view engine','ejs');
 
 app.get('/',function(req,res,params){
-	
+	res.render("index");
 })
 
 app.listen(process.env.PORT || 3000, function(){
