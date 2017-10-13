9c3e8dfc81b3a024c626f56913451f4a9d7f8dae
Husain Zaidi <husainhz7@gmail.com>
2017-10-13 21:36:43 +0000

    added index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get('/',function(req,res,params){
+	
+})
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log("HEllo")
+})
